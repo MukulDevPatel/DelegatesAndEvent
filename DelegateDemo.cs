755d9c98d012c9e8ed34c131e69c2d89b5abef56
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-25 19:43:33 +0530

    [Mukul Dev] Add: This is Delegate and Event

---
+++ DelegateDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class DelegateDemo
+    {
+        public class SingleCastDelegate
+        {
+            public int Addition(int x, int y)
+            {
+                int a = 1;
+                int b = 2;
+                return a + b;
+            }
+            public void Run()
+            {
+                Console.WriteLine(Addition(1, 2));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static DelegatesAndEvents.DelegateDemo;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number for execution");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n1. Single cast delegate\n2. Exit");
+                int select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+
+                    case 1:
+                        SingleCastDelegate single = new SingleCastDelegate();
+                        single.Run();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+                
+            }
+        }
+    }
+}
